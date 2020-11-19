f4d1c759181e29283a6cef5e1f7595a288e6543e
Fríðbjartur Miðberg <fridbjartur@Fribjarturs-MacBook-Pro.local>
2020-11-19 15:10:46 +0100

    search bar tags v2

--- app.js
+++ app.js
@@ -32,9 +32,37 @@ const searchSuggestions = ()=>{
   
 const addTagsToSearch = ()=>{
   let btn = event.target;
-  tagsContainer.insertAdjacentHTML('afterbegin', `<button>${btn.textContent}</button>`);
+  tagsContainer.insertAdjacentHTML('beforeend', `<button>${btn.textContent}</button>`);
   search.style.paddingLeft = `${tagsContainer.offsetWidth}px`;
+};
+const removeTagsFromSearch = ()=>{
+  // let aTags = document.querySelectorAll('#search-input-tags button');
+  // console.log(aTags);
 };
 
   searchSuggestions();
+  removeTagsFromSearch();
   // addTagsToSearch();
+
+  search.addEventListener('keydown', function (event) {
+    if (event.keyCode == 8 && search.value == '') {
+      let aTags = document.querySelectorAll('#search-input-tags button');
+      if(! aTags.length == 0){
+        aTags[aTags.length - 1].remove();
+        search.style.paddingLeft = `${tagsContainer.offsetWidth}px`;
+      }
+        // Call event.preventDefault() to stop the character before the cursor
+        // from being deleted. Remove this line if you don't want to do that.
+        // event.preventDefault();
+    }
+});
+
+search.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13 && search.value.length > 0) {
+    tagsContainer.insertAdjacentHTML('beforeend', `<button>${search.value}</button>`);
+    search.style.paddingLeft = `${tagsContainer.offsetWidth}px`;
+    search.value = '';
+    // Cancel the default action, if needed
+    event.preventDefault();
+  }
+});
